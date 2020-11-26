0b6fe3602de23abc1b2f817ce5732dd5fb085e45
loricode <loricode2793@gmail.com>
2020-11-26 16:51:59 -0500

    segundo commit

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -6,17 +6,35 @@ const baseURL = process.env.REACT_APP_API_URL //npm i dotenv
 
 function Home() {
 
-const  [listTeam, setListTeam ] = useState([])
+const [ listTeam, setListTeam ] = useState([])
+const [ name, setName ] = useState('')
+const [ trophy, setTrophy ] = useState('')
+const [ image, setImage ] = useState('')
 
   useEffect(() => {
      getTeams() 
   },[])
 
-  async function getTeams(){
-     const { data } = await axios.get(baseURL)
-     console.log(data)
-     setListTeam(data)
-  } 
+ async function getTeams(){
+    const { data } = await axios.get(baseURL)
+    setListTeam(data)
+ } 
+
+ async function saveTeam(){
+    const obj = { name, trophy, image }
+    const { data } = await axios.post(baseURL, obj)
+    console.log(data)
+    clearInput()
+    getTeams()
+ }
+
+ function clearInput(){
+    setName('')
+    setTrophy('')
+    setImage('')
+ }
+
+ function updateTeam(){ }
 
 
     return (
@@ -27,19 +45,32 @@ const  [listTeam, setListTeam ] = useState([])
                         <input type="text"
                                placeholder="Name"
                                className="form-control mt-2"
+                               value={name}
+                               onChange={ e => setName(e.target.value)} 
                         />
                         <input type="text"
                                placeholder="Trophy"
                                className="form-control mt-2"
+                               value={trophy}
+                               onChange={ e => setTrophy(e.target.value)} 
                         />
 
                         <input type="text"
                                placeholder="Image"
                                className="form-control mt-2 mb-2"
+                               value={image}
+                               onChange={ e => setImage(e.target.value)} 
                         />
-                        <button className="btn btn-secondary">
+                        
+                        <button className="btn btn-secondary mb-2 "
+                                onClick={saveTeam}>
                             Save
                         </button>
+                        <button className="btn btn-dark "
+                                onClick={updateTeam}>
+                            Update
+                        </button>
+                                           
                     </div>
                 </div>
                  
